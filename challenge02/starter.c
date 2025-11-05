d0720735069544b9f97a0882470d5c158519d524
rodri2253 <rodrigomansergas@gmail.com>
2025-11-05 14:53:00 +0100

    Challenge 2 OK

--- challenge02/starter.c
+++ challenge02/starter.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
-int suma(int a, int b);
-int resta(int a, int b);
-int multiplicacion(int a, int b);
-int division(int a, int b);
+int suma(int a, int b)
+{
+    return a + b;
+}
+int resta(int a, int b)
+{
+    return a - b;
+}
+int multiplicacion(int a, int b)
+{
+    return a * b;
+}
+int division(int a, int b){
+    if (b != 0) {
+        return a / b;
+    } else
+    {
+        printf("Error: Division por cero.\n");
+        return 0; // Retorna 0 en caso de division por cero
+    }
+    
+}
 
 int main() {
     printf("Calculadora inicializada.\n");
