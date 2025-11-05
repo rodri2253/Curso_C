7a5dc46a446b64f7dc0e00b201353997f4200af5
rodri2253 <rodrigomansergas@gmail.com>
2025-11-05 13:55:21 +0100

    CHALLENGE 2 OK

---
+++ challenge02/test_challenge02.c
@@ -0,0 +1,46 @@
+int suma(int a, int b) {
+    return a + b;
+}
+int resta(int a, int b) {
+    return a - b;
+}
+int multiplicacion(int a, int b) {
+    return a * b;
+}
+int division(int a, int b) {
+    if (b == 0) {
+        return 0; // Manejo simple de división por cero
+    }
+    return a / b;
+}
+
+//testeo de la funcion suma
+#include <assert.h>
+#include <stdio.h>
+void test_de_todo() {
+    assert(suma(2, 3) == 5);
+    assert(suma(-1, 1) == 0);
+    assert(suma(-2, -3) == -5);
+
+//testeo de la funcion resta
+    assert(resta(5, 3) == 2);
+    assert(resta(0, 1) == -1);
+    assert(resta(-2, -3) == 1);
+
+//testeo de la funcion multiplicacion
+    assert(multiplicacion(2, 3) == 6);
+    assert(multiplicacion(-1, 1) == -1);
+    assert(multiplicacion(-2, -3) == 6);
+//testeo de la funcion division
+    assert(division(6, 3) == 2);
+    assert(division(5, 0) == 0); // División por cero
+    assert(division(-6, -3) == 2);
+
+
+    printf("TODO OK\n");
+}
+int main()
+{
+    test_de_todo();
+    return 0;
+}  
